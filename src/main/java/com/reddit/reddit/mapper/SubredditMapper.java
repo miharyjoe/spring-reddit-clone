f67b936aef25d17c23f85aa685d6801788bfaca7
miharyjoel <miharyjoel@gmail.com>
2023-11-09 21:42:46 +0300

    feat: use mapstruct for mapper

---
+++ src/main/java/com/reddit/reddit/mapper/SubredditMapper.java
@@ -0,0 +1,26 @@
+package com.reddit.reddit.mapper;
+
+
+import com.reddit.reddit.dto.SubredditDto;
+import com.reddit.reddit.model.Post;
+import com.reddit.reddit.model.Subreddit;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface SubredditMapper {
+  @Mapping(target = "numberOfPosts", expression = "java(mapPosts(subreddit.getPosts()))")
+
+  SubredditDto mapSubredditToDto(Subreddit subreddit);
+
+  default Integer mapPosts(List<Post> numberOfPosts) {
+    return numberOfPosts.size();
+  }
+  @InheritInverseConfiguration
+  @Mapping(target = "posts", ignore = true)
+  Subreddit mapDtoToSubreddit(SubredditDto subredditDto);
+}

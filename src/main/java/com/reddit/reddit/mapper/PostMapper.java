576345b13c8018aef720272173a97630cb20d897
miharyjoel <miharyjoel@gmail.com>
2024-01-14 13:51:16 +0300

    fix: post mapper

--- src/main/java/com/reddit/reddit/mapper/PostMapper.java
+++ src/main/java/com/reddit/reddit/mapper/PostMapper.java
@@ -9,15 +9,17 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
-public interface PostMapper {
+public abstract class  PostMapper {
 
   @Mapping(target = "createdDate", expression = "java(java.time.Instant.now())")
   @Mapping(target = "description", source = "postRequest.description")
-  Post map(PostRequest postRequest, Subreddit subreddit, User user);
+  @Mapping(target = "subreddit", source = "subreddit")
+  @Mapping(target = "user", source = "user")
+  public abstract Post map(PostRequest postRequest, Subreddit subreddit, User user);
 
   @Mapping(target = "id", source = "postId")
   @Mapping(target = "subredditName", source = "subreddit.name")
   @Mapping(target = "userName", source = "user.username")
-  PostResponse mapToDto(Post post);
+  public abstract PostResponse mapToDto(Post post);
 
 }

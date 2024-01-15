c38ff7a113aa4516a3a60fab2acae4d73908aa2b
miharyjoel <miharyjoel@gmail.com>
2024-01-15 22:06:47 +0300

    fix: subreddit numbers of post mapper

--- src/main/java/com/reddit/reddit/mapper/SubredditMapper.java
+++ src/main/java/com/reddit/reddit/mapper/SubredditMapper.java
@@ -4,22 +4,27 @@ package com.reddit.reddit.mapper;
 import com.reddit.reddit.dto.SubredditDto;
 import com.reddit.reddit.model.Post;
 import com.reddit.reddit.model.Subreddit;
+import com.reddit.reddit.repository.PostRepository;
 import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.springframework.beans.factory.annotation.Autowired;
 
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
-public interface SubredditMapper {
-  @Mapping(target = "numberOfPosts", expression = "java(mapPosts(subreddit.getPosts()))")
-  SubredditDto mapSubredditToDto(Subreddit subreddit);
+public abstract class SubredditMapper {
 
-  default Integer mapPosts(List<Post> numberOfPosts) {
-    return numberOfPosts.size();
+  @Autowired
+  private PostRepository postRepository;
+  @Mapping(target = "numberOfPosts", expression = "java(mapPosts(subreddit))")
+  public abstract SubredditDto mapSubredditToDto(Subreddit subreddit);
+
+  Integer mapPosts(Subreddit subreddit) {
+    return postRepository.findAllBySubreddit(subreddit).size();
   }
   @InheritInverseConfiguration
   @Mapping(target = "posts", ignore = true)
-  Subreddit mapDtoToSubreddit(SubredditDto subredditDto);
+  public abstract  Subreddit mapDtoToSubreddit(SubredditDto subredditDto);
 }

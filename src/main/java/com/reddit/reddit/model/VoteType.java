f9d708d81dd19ff73e2377699998e8ec4ed0d37a
miharyjoel <miharyjoel@gmail.com>
2024-01-15 18:56:27 +0300

    feat: impl vote system

--- src/main/java/com/reddit/reddit/model/VoteType.java
+++ src/main/java/com/reddit/reddit/model/VoteType.java
@@ -1,11 +1,5 @@
 package com.reddit.reddit.model;
 
-import jakarta.persistence.Entity;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 public enum VoteType {
   UPVOTE(1),DOWNVOTE(-1);
 
--- src/main/java/com/reddit/reddit/repository/VoteRepository.java
+++ src/main/java/com/reddit/reddit/repository/VoteRepository.java
@@ -1,9 +1,14 @@
 package com.reddit.reddit.repository;
 
+import com.reddit.reddit.model.Post;
+import com.reddit.reddit.model.User;
 import com.reddit.reddit.model.Vote;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface VoteRepository extends JpaRepository<Vote, Long> {
+  Optional<Vote> findTopByPostAndUserOrderByVoteIdDesc(Post post, User currentUser);
 }

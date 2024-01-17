a5cdfd9544ce2e1d87ad30ec93af9b1593b1f442
miharyjoel <miharyjoel@gmail.com>
2024-01-17 17:43:03 +0300

    feat: add springdoc opneApi

--- src/main/java/com/reddit/reddit/RedditApplication.java
+++ src/main/java/com/reddit/reddit/RedditApplication.java
@@ -1,11 +1,14 @@
 package com.reddit.reddit;
 
+import com.reddit.reddit.config.OpenApiConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
+@Import(OpenApiConfiguration.class)
 public class RedditApplication {
 
 	public static void main(String[] args) {
